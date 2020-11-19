c2eaaea06ccf7fb835b7b513051e661712e19078
9A713018 <72920081+9A713018@users.noreply.github.com>
2020-11-19 19:23:59 +0800

    Add files via upload

---
+++ WP_2020_01_HW02_9A713018/WP_2020_01_HW02_9A713018/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WP_2020_01_HW02_9A713018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonGO_Click(object sender, EventArgs e)
+        {
+            Poker p = new Poker();
+            List<int> poker = p.GetPoker(52);
+
+            string msg = "*";
+            for (int i =0 ; i < poker.Count; i++)
+            {
+                msg += $"{poker[i]} ,";
+            }
+
+            rtbMsg.Text = msg + "*";
+        }
+    }
+}
